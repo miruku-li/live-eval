5ffed10eb67ecb1f41060537246f5b602a96ee3c
tscherter <vincent@tscherter.net>
2020-10-29 11:42:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('miruku-li/live-eval - helo 2')
+import m from '/vendor/mithril.js'
+import b from '/vendor/bss.js'
+
+let cur = '', evaluated, update = (value) => {
+  try {
+    evaluated = JSON.stringify(eval(cur = value))
+  } catch (ex) {
+    evaluated = ex.message
+  }
+};
+
+let index  = {
+  view: () => m('div'+b`d flex; fd column;border 1px solid silver`, 
+    m('input', { 
+      oninput: ({target}) => update(target.value)
+    }), 
+    m('pre', evaluated)
+  )
+}
+
+m.mount(document.body, index)
