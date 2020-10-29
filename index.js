0d7b044614b7ebee3e5f0742b58ff096bff830b7
Vincent Tscherter <vincent@tscherter.net>
2020-10-29 19:22:59 +0100

    test

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-import c from '../vendor/crelt.js'
+import c from "../vendor/crelt.js";
 
 class LiveEval extends HTMLElement {
-  
+  // test
   constructor() {
     super();
-    const update = (v) => {
-      value = v; try {
-        pre.innerHTML = eval(v)
+    const update = v => {
+      value = v;
+      try {
+        pre.innerHTML = eval(v);
       } catch (ex) {
-        pre.innerHTML = ex.message
+        pre.innerHTML = ex.message;
       }
-    }
-    let shadow = this.attachShadow({mode: 'open'});
-    let value = 'bar', pre
-    shadow.appendChild(c('div', {
-        style: 'display:flex;flex-direction: column;border: 1px solid silver'
-      },
-      c('input', {
-        value,
-        oninput: ({target}) => update(target.value)
-      }), 
-      c('pre')
-    ))
-    pre = shadow.querySelector('pre')
+    };
+    let shadow = this.attachShadow({ mode: "open" });
+    let value = "bar",
+      pre;
+    shadow.appendChild(
+      c(
+        "div",
+        {
+          style: "display:flex;flex-direction: column;border: 1px solid silver"
+        },
+        c("input", {
+          value,
+          oninput: ({ target }) => update(target.value)
+        }),
+        c("pre")
+      )
+    );
+    pre = shadow.querySelector("pre");
   }
-
 }
-customElements.define('mirukuli-live-eval', LiveEval);
+customElements.define("mirukuli-live-eval", LiveEval);
